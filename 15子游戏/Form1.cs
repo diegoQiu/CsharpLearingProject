1ceadbc951ff84c9ffe42a5a3a2efe0b50451494
diegoQiu <siwangrufeng666@gmail.com>
2018-03-12 23:51:35 +0800

    十五子游戏下个版本准备做成绝句诗拼凑

---
+++ 15子游戏/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _15子游戏
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int N = 4;
+        Button[,] buttons = new Button[N, N];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //产生所有按钮
+            GenerateAllButtons();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //打乱顺序
+            Shuffle();
+        }
+
+        void Shuffle()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int a = rnd.Next(N);
+                int b = rnd.Next(N);
+                int c = rnd.Next(N);
+                int d = rnd.Next(N);
+                Swap(buttons[a, b], buttons[c, d]);
+            }
+        }
+
+        void Swap(Button btna, Button btnb)
+        {
+            //交换文本信息
+            string str = btna.Text;
+            btna.Text = btnb.Text;
+            btnb.Text = str;
+
+            //交换可见性
+            bool v = btna.Visible;
+            btna.Visible = btnb.Visible;
+            btnb.Visible = v;
+        }
+
+        void GenerateAllButtons()
+        {
+            int x0 = 100, y0 = 10, w = 45, d = 50;
+            for (int r = 0; r < N; r++)
+            {
+                for (int c = 0; c < N; c++)
+                {
+                    int num = r * N + c;
+                    Button btn = new Button();
+                    btn.Text = (num + 1).ToString();
+                    btn.Top = y0 + r * d;
+                    btn.Left = x0 + c * d;
+                    btn.Width = w;
+                    btn.Height = w;
+                    btn.Visible = true;
+                    btn.Tag = r * N + c;//这个数据用来表示他行列位置
+
+                    //注册事件
+                    btn.Click += new EventHandler(btn_Click);
+                    buttons[r, c] = btn;//放在数组中
+                    this.Controls.Add(btn);//加到界面上
+                }
+            }
+          
+            buttons[N - 1, N - 1].Visible = false;//最后一个不显示
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;//当前点中的按钮
+            Button blank = findHiddenButton();//空白按钮
+
+            if (IsNeighbor(btn, blank))
+            {
+                Swap(btn, blank);
+                blank.Focus();
+            }
+
+            if (ResultIsOk())
+            {
+                MessageBox.Show("ok");
+            }
+            int a = (int)btn.Tag;
+            
+        }
+
+        Button findHiddenButton()
+        {
+            for (int r = 0; r < N; r++)
+            {
+                for (int c = 0; c < N; c++)
+                {
+                    if (!buttons[r, c].Visible)
+                        return buttons[r, c];
+                }
+            }
+            return null;
+        }
+
+        bool IsNeighbor(Button btnA, Button btnB)
+        {
+            int a = (int)btnA.Tag;
+            int b = (int)btnB.Tag;
+            int r1 = a / N, c1 = a % N;
+            int r2 = b / N, c2 = b % N;
+
+            if (r1 == r2 && (c1 == c2 - 1 || c1 == c2 + 1) || c1 == c2 && (r1 == r2 - 1 || r1 == r2 + 1))
+                return true;
+            else return false;
+        }
+
+        bool ResultIsOk()
+        {
+            for (int r = 0; r < N; r++)
+            {
+                for (int c = 0; c < N; c++)
+                {
+                    if (buttons[r, c].Text != (r * N + c + 1).ToString())
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnJudge = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.lstDisp = new System.Windows.Forms.ListBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblOp = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(29, 122);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 0;
+            this.btnNew.Text = "出题";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnJudge
+            // 
+            this.btnJudge.Location = new System.Drawing.Point(166, 122);
+            this.btnJudge.Name = "btnJudge";
+            this.btnJudge.Size = new System.Drawing.Size(75, 23);
+            this.btnJudge.TabIndex = 1;
+            this.btnJudge.Text = "判分";
+            this.btnJudge.UseVisualStyleBackColor = true;
+            this.btnJudge.Click += new System.EventHandler(this.btnJudge_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(29, 84);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(145, 21);
+            this.txtAnswer.TabIndex = 2;
+            // 
+            // lstDisp
+            // 
+            this.lstDisp.FormattingEnabled = true;
+            this.lstDisp.ItemHeight = 12;
+            this.lstDisp.Location = new System.Drawing.Point(25, 172);
+            this.lstDisp.Name = "lstDisp";
+            this.lstDisp.Size = new System.Drawing.Size(215, 64);
+            this.lstDisp.TabIndex = 3;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(35, 40);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(0, 12);
+            this.lblA.TabIndex = 4;
+            // 
+            // lblOp
+            // 
+            this.lblOp.AutoSize = true;
+            this.lblOp.Location = new System.Drawing.Point(82, 40);
+            this.lblOp.Name = "lblOp";
+            this.lblOp.Size = new System.Drawing.Size(0, 12);
+            this.lblOp.TabIndex = 5;
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(133, 40);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(0, 12);
+            this.lblB.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(180, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblOp);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lstDisp);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnJudge);
+            this.Controls.Add(this.btnNew);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnJudge;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.ListBox lstDisp;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblOp;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
